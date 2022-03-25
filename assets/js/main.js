d03f37e3c717c307f04dae5eff5ec7ff29002386
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-03-25 17:45:34 +0100

    create function km

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
+// Definisco la funzione per estrapolare il nome
 function user_name() {
-    let x = document.getElementById("my_name").value;
-    document.getElementById("passenger_name").innerHTML = x;
+    let passenger_name = document.getElementById("my_name").value;
+    document.getElementById("passenger_name").innerHTML = passenger_name;
+}
+
+// creo variabili per il prezzo del biglietto
+function km() {
+    let ticket_price = document.getElementById("km_to_travel_form").value * 0.21;
+    document.getElementById("ticket_price").innerHTML = ticket_price;
 }
