db48cd62f79072dc05777d0cf58e0bf113ebea17
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-03-25 18:01:45 +0100

    create age function

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,9 @@ function km() {
     let ticket_price = document.getElementById("km_to_travel_form").value * 0.21;
     document.getElementById("ticket_price").innerHTML = ticket_price;
 }
+
+function age() {
+    let user_age = document.getElementById("form_passenger_age").value;
+    document.getElementById("discount").innerHTML = user_age;
+}
+
